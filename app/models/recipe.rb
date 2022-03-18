4233efd81a6b2887b07c6baf5b9dd8b4f2b9c4f3
chall6753 <chall70101@gmail.com>
2022-03-18 16:20:01 -0600

    accept nested attributes and validate uniqueness

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,4 +3,8 @@ class Recipe < ApplicationRecord
   has_many :comments
   has_many :recipe_ingredients
   has_many :ingredients, through: :recipe_ingredients
+
+  accepts_nested_attributes_for :recipe_ingredients
+
+  validates :name, uniqueness: {case_sensitive: false, scope: :user_id} 
 end

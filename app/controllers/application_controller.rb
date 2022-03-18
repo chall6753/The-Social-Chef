7683fe625d08146741b9367ef2c33322f6bd7e2a
chall6753 <chall70101@gmail.com>
2022-03-18 16:14:57 -0600

    add comments for understanding

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,11 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
-  before_action :authorize
+
+  before_action :authorize #before all controller actions run the authorize method will run
+
+  #checks to see if a user is logged in by checking if the session has a user_id thats not nil
   def authorize 
     return render json: {error: 'Not authorized'}, status: :unauthorized unless session.include? :user_id
   end
+
 end

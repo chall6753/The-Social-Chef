0a5230c9133590d1bdf6d7397e84ee369ea39983
chall6753 <chall70101@gmail.com>
2022-03-25 21:36:27 -0600

    add dependent destroy

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
     #Associations
-    has_many :recipes 
-    has_many :comments
+    has_many :recipes, dependent: :destroy 
+    has_many :comments, dependent: :destroy
     has_secure_password
 
     #validations

a2ad84e95d69915d25e2560b67cdf45c25978631
chall6753 <chall70101@gmail.com>
2022-03-25 21:41:43 -0600

    add recipe create component

--- client/src/components/recipes/RecipeCreate.js
+++ client/src/components/recipes/RecipeCreate.js
@@ -1,30 +1,34 @@
 import React, {useState, useEffect}from 'react'
-import {Container} from 'react-bootstrap'
+import {useNavigate} from 'react-router-dom'
+import {Container, Form, Card} from 'react-bootstrap'
 import AddIngredient from './AddIngredient'
 
-function RecipeCreate({handleCreateRecipe}){
-    const[username,setUsername]=useState('bobby')
-    const[recipeName,setRecipeName]=useState('soup')
+function RecipeCreate({recipes, setRecipes}){
+    
+    const[recipeName,setRecipeName]=useState('')
     const[recipeIngredients,setRecipeIngredients]=useState([])
     const[instructions,setInstructions]=useState('')
+    const navigate = useNavigate()
     
     function createRecipe(e){
         e.preventDefault()
-        fetch('http://localhost:9292/recipe_create', {
+        fetch('/recipes', {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
             },
             body: JSON.stringify({
-                username: username,
-                recipeName: recipeName,
-                recipeIngredients: recipeIngredients,
+                name: recipeName,
+                recipe_ingredients: recipeIngredients,
                 instructions: instructions
             }),
         })
-        .then((r)=>r.json())
-        .then((recipe) => handleCreateRecipe(recipe))
-        
+        .then((res)=>{
+            if (res.ok){
+                navigate('/recipes') //once the recipe submit button and succesful creation of recipe navigate to recipe list
+                res.json().then((recipe)=> setRecipes([...recipes,recipe])) //updates recipes state so page rerenders with new recipe in the list
+            }
+        })
         }
     
         function addIngredient(ingredientName,quantity,unit){
@@ -32,30 +36,38 @@ function RecipeCreate({handleCreateRecipe}){
             let updatedIngredients = [...recipeIngredients,ingredient]
             setRecipeIngredients(updatedIngredients)
         }
-        
+        console.log(recipeIngredients)
     return (
         <Container>
-            
+            <Card>
             <h1>Create New Recipe</h1>
-            <form>
-                <label>Chef: </label>
-                <input type='text' onChange={(e)=>setUsername(e.target.value)}></input>
-                <label>Recipe Name</label>
-                <input type="text" onChange={(e)=>setRecipeName(e.target.value)}></input>
-                <br></br>
-                <h2>Ingredients</h2>
-                <AddIngredient addIngredient={addIngredient}/>
-                <ul>
-                {recipeIngredients.length>0 && 
-                    recipeIngredients.map((ingredient)=>{
-                        return <li key={ingredient.ingredient}>{ingredient.ingredient} {ingredient.quantity} {ingredient.unit}</li>
-                    }) 
-                }
-                </ul>
-                <h2>Instructions</h2>
-                <label>Cooking Instructions: </label>
-                <textarea type='paragraph_text' cols='80' onChange={(e)=>setInstructions(e.target.value)}></textarea>
-            </form>  
+                <Form>
+                    
+                    <label>Recipe Name</label>
+                    <input type="text" onChange={(e)=>setRecipeName(e.target.value)}></input>
+                </Form>
+            </Card>
+            <Card>
+                <Form>
+                    <h2>Ingredients</h2>
+                    <AddIngredient addIngredient={addIngredient}/>
+                    <ul>
+                    {recipeIngredients.length>0 && 
+                        recipeIngredients.map((ingredient)=>{
+                            return <li key={ingredient.ingredient}>{ingredient.ingredient} {ingredient.quantity} {ingredient.unit}</li>
+                        }) 
+                    }
+                    </ul>
+                </Form>
+            </Card>
+                <Card>
+                    <Form>
+                        <h2>Instructions</h2>
+                        <label>Cooking Instructions: </label>
+                        <textarea type='paragraph_text' cols='80' onChange={(e)=>setInstructions(e.target.value)}></textarea>
+                    </Form>
+                </Card>
+                
             <button type='submit' onClick={createRecipe}>Submit Recipe</button>          
         
         </Container>

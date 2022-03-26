3a456fd6e3fad2c76526c39d88d11f2e2368626c
chall6753 <chall70101@gmail.com>
2022-03-25 21:35:44 -0600

    change error message

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
             session[:user_id] = user.id
             render json: user, status: :created
         else
-            render json: {errors: "Unauthorized"}, status: :unauthorized
+            render json: {errors: "Incorrect username password combo"}, status: :unauthorized
         end
     end
 

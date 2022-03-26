f420e46283cb076316e838a0a02e5f7f266b0a23
chall6753 <chall70101@gmail.com>
2022-03-25 21:40:40 -0600

    add conditional rendering for logged in

--- client/src/components/navigation/NavbarSide.js
+++ client/src/components/navigation/NavbarSide.js
@@ -2,28 +2,41 @@ import React from "react"
 import {NavLink} from 'react-router-dom'
 import {Navbar, Nav, Container} from 'react-bootstrap'
 
-function NavbarSide(){
+function NavbarSide({currentUser}){
 
-    return(
-      <div>
-          <Navbar className='sidenav'>
-                <Nav>
-                    <ul>
-                    <NavLink className='sidenavLink' to='/'>Home</NavLink>       
-                    <NavLink className='sidenavLink' to="/recipes" exact="true">Recipes</NavLink>
-                    <NavLink className='sidenavLink' to="/chefs" exact="true">Chefs</NavLink>
-                    <NavLink className='sidenavLink' to='/login' exact='true'>Login</NavLink> 
-                    </ul>
-                </Nav> 
-                
-            </Navbar>
-      </div>  
-            
-            
-            
-
-       
-    )
+    if (currentUser != ''){
+        return(
+            <div>
+                <Navbar className='sidenav'>
+                    <Nav>
+                        <ul>
+                        <NavLink className='sidenavLink' to='/'>Home</NavLink>       
+                        <NavLink className='sidenavLink' to="/recipes" exact="true">Recipes</NavLink>
+                        <NavLink className='sidenavLink' to="/chefs" exact="true">Chefs</NavLink>
+                        <NavLink className='sidenavLink' to='/recipes/create' exact='true'>Share Recipe</NavLink>
+                        </ul>
+                    </Nav> 
+                </Navbar>
+            </div>        
+        )
+    }
+    else{
+        return(
+            <div>
+            <Navbar className='sidenav'>
+                    <Nav>
+                        <ul>
+                        <NavLink className='sidenavLink' to='/'>Home</NavLink>       
+                        <NavLink className='sidenavLink' to="/recipes" exact="true">Recipes</NavLink>
+                        <NavLink className='sidenavLink' to="/chefs" exact="true">Chefs</NavLink>
+                        </ul>
+                    </Nav> 
+                    
+                </Navbar>
+            </div>         
+        )
+    }
+   
 }
 
 export default NavbarSide

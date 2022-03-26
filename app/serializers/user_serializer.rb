174bfde635cc04686ebbaf0be9dc9e0f8216544a
chall6753 <chall70101@gmail.com>
2022-03-25 21:37:49 -0600

    add bio

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,6 +1,6 @@
 class UserSerializer < ActiveModel::Serializer
   
-  attributes :id, :username, :first_name, :last_name, :email, :password_digest
+  attributes :id, :username, :first_name, :last_name, :email, :password_digest, :bio
 
   has_many :recipes
 end

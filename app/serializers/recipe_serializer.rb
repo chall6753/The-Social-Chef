d8ba7451b9973e7742f0e1a2965bb0f20c51262f
chall6753 <chall70101@gmail.com>
2022-03-25 21:37:19 -0600

    add comments and order desc

--- app/serializers/recipe_serializer.rb
+++ app/serializers/recipe_serializer.rb
@@ -1,6 +1,13 @@
 class RecipeSerializer < ActiveModel::Serializer
+  
   attributes :id, :name, :description, :instructions
 
   belongs_to :user
+  
   has_many :recipe_ingredients
+  has_many :comments do 
+    object.comments.order(created_at: :desc) #newest comments first
+  end
+
+  
 end

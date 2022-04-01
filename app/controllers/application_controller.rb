607041a5ec9aba82828c6f38d89410513dfce394
chall6753 <chall70101@gmail.com>
2022-04-01 10:00:39 -0600

    give current_user method to all inherited models

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,7 @@ class ApplicationController < ActionController::API
     return render json: {error: 'Not authorized'}, status: :unauthorized unless session.include? :user_id
   end
 
+  def current_user
+    User.find_by(id: session[:user_id])
+  end
 end

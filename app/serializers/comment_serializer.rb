9a059b58bfa6d5a54b852c96e1d2566b77025049
chall6753 <chall70101@gmail.com>
2022-04-01 14:26:06 -0600

    added attribute if user can modify comment

--- app/serializers/comment_serializer.rb
+++ app/serializers/comment_serializer.rb
@@ -1,6 +1,13 @@
 class CommentSerializer < ActiveModel::Serializer
-  attributes :comment, :rating
+  attributes :id, :comment, :rating, :can_modify_comment
   has_one :user
   has_one :recipe
 
+  def can_modify_comment
+    if current_user
+      can_modify = current_user.id == self.object.user_id
+    else 
+      can_modify = false
+    end
+  end
 end

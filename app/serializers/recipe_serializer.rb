7f341b444bbee7a8b8903c5bf5fc34396bba1f36
chall6753 <chall70101@gmail.com>
2022-04-06 17:11:40 -0600

    add can_modify method

--- app/serializers/recipe_serializer.rb
+++ app/serializers/recipe_serializer.rb
@@ -1,6 +1,6 @@
 class RecipeSerializer < ActiveModel::Serializer
   
-  attributes :id, :name, :description, :instructions
+  attributes :id, :name, :description, :instructions, :can_modify_recipe
 
   belongs_to :user
   
@@ -9,5 +9,12 @@ class RecipeSerializer < ActiveModel::Serializer
     object.comments.order(updated_at: :desc) #newest comments first
   end
 
+  def can_modify_recipe
+    if current_user
+      can_modify = current_user.id == self.object.user_id
+    else 
+      can_modify = false
+    end
+  end
   
 end

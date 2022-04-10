4cc0cb43838efad9c4d20bc05aae9820420b7c62
chall6753 <chall70101@gmail.com>
2022-04-10 13:44:54 -0600

    add class method to create new recipe_ingredient

--- app/models/recipe_ingredient.rb
+++ app/models/recipe_ingredient.rb
@@ -1,4 +1,11 @@
 class RecipeIngredient < ApplicationRecord
   belongs_to :recipe
   belongs_to :ingredient
+
+  def self.new_recipe_ingredient(recipe_ingredient_params,recipe)
+    recipe_ingredient_params[:recipe_ingredients].map do |ingredient|     #map through the array of recipe ingredient objects
+      ingredient_id = Ingredient.find_or_create_by(name: ingredient[:ingredient]).id      
+      recipe.recipe_ingredients.create(ingredient_id: ingredient_id, quantity: ingredient[:quantity], unit: ingredient[:unit] )
+    end
+  end
 end

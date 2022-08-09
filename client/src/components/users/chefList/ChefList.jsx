851234add00533674e59bacd785eaad649467293
chall6753 <chall70101@gmail.com>
2022-08-09 15:53:09 -0600

    add search bar

--- client/src/components/users/chefList/ChefList.jsx
+++ client/src/components/users/chefList/ChefList.jsx
@@ -1,19 +1,41 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import ChefCard from '../chefCard/ChefCard'
 import './chefList.scss'
 function ChefList({users}) {
   const [filteredUsers, setFilteredUsers] = useState(users)
+  const [search, setSearch] = useState('')
+
+  useEffect(()=>{
+    setFilteredUsers(users)
+  },[users])
+
+  const handleSearch = (e)=> {
+    setSearch(e.target.value)
+  }
+
+  useEffect(()=>{
+    setFilteredUsers(users.filter((user)=>{
+      if (search ===''){
+        return user
+      }
+      else{
+        return user.username.toLowerCase().includes(search)
+      }
+    }))
+    
+  },[search])
+  
 
   return (
     <div className='chef-list-container'>
       <div className="top">
-         <h1>Chef List</h1>
+        <h1>Chef List</h1>
         <label>search</label>
-        <input type="text" />
+        <input onChange={handleSearch} type="text"/>
       </div>
      
       <ul>
-        {users.map(user => {
+        {filteredUsers.slice(0,15).map(user => {
           return <ChefCard key={user.id} user={user}/>  
         })
         }
